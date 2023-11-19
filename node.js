4f3fb90f5e1e61e7507572ec5898428693df2de5
varunrvkmr <35124671+varunrvkmr@users.noreply.github.com>
2023-11-19 15:22:15 -0700

    Added protected route and Logout functionality
    Added protected route and Logout functionality as well as changes to some redirects

--- node.js
+++ node.js
@@ -3,6 +3,7 @@ const express = require('express');
 const sql = require('mssql');
 const path = require('path'); 
 const app = express();
+const crypto = require('crypto');
 
 
 app.use(express.static('public'));
@@ -20,6 +21,32 @@ const config = {
         trustServerCertificate: true 
     }
 };
+
+
+function createJWT(payload, secret) {
+    const header = Buffer.from(JSON.stringify({ alg: 'HS256', typ: 'JWT' })).toString('base64');
+    const encodedPayload = Buffer.from(JSON.stringify(payload)).toString('base64');
+    const signature = crypto.createHmac('sha256', secret).update(`${header}.${encodedPayload}`).digest('base64');
+    return `${header}.${encodedPayload}.${signature}`;
+}
+
+function verifyJWT(token, secret) {
+    const [header, payload, signature] = token.split('.');
+    const expectedSignature = crypto.createHmac('sha256', secret).update(`${header}.${payload}`).digest('base64');
+    return signature === expectedSignature;
+}
+
+const secretKey = 'your-secret-key'; // Use a strong, secret key
+
+function isAuthenticated(req, res, next) {
+    const token = req.headers.authorization;
+    if (token && verifyJWT(token.split(' ')[1], secretKey)) {
+        return next();
+    }
+    return res.status(401).send('Unauthorized');
+}
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'LoginPage.html'));
 });
@@ -28,9 +55,6 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'registration.html'));
 });
 
-app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public2', 'index.html'));
-});
 
 app.post('/submit-form', async (req, res) => {
     
@@ -38,8 +62,6 @@ app.post('/submit-form', async (req, res) => {
 
     
     const { username, dob, hometown, gender, password } = req.body;
-
-    
 
     try {
         await sql.connect(config);
@@ -57,7 +79,7 @@ app.post('/submit-form', async (req, res) => {
         request.input('password', sql.VarChar, password); 
 
         await request.query(insertQuery);
-        res.status(200).send('Data inserted');
+        res.redirect('http://localhost:8000/');
     } catch (err) {
         console.error(err);
         res.status(500).send('Server error: ' + err.message);
@@ -66,9 +88,10 @@ app.post('/submit-form', async (req, res) => {
     }
 });
 
+
 app.post('/login', async (req, res) => {
   
-    console.log(req.body);
+    //console.log(req.body);
 
     
     const { username, password } = req.body;
@@ -94,12 +117,37 @@ app.post('/login', async (req, res) => {
             const isMatch = password === storedPassword; 
 
             if (isMatch) {
-                res.redirect('https://main.dzyoo64rgfqyj.amplifyapp.com/#');
-            } else {
+                // Create JWT
+                const token = createJWT({ username }, secretKey);
+                
+                await sql.connect(config);
+                const updateTokenQuery = `
+                    UPDATE [dbo].[mock_Accounts]
+                    SET token = @token
+                    WHERE username = @username
+                    `;
+
+        
+                const tokenRequest = new sql.Request();
+                tokenRequest.input('username', sql.VarChar, username);
+                tokenRequest.input('token', sql.VarChar, token);
+                await tokenRequest.query(updateTokenQuery);
+                
+                const redirectUrl = `http://localhost:8000/protected-route?token=${encodeURIComponent(token)}`;
+                //const redirectUrl = `https://main.dzyoo64rgfqyj.amplifyapp.com/protected-route?token=${encodeURIComponent(token)}`;
+                
+                res.redirect(redirectUrl);
+                
+                
+                //res.redirect('http://localhost:8000/protected-route');
+            //if (isMatch) {
+                //res.redirect('http://localhost:8000/index');
+              //  res.redirect('https://main.dzyoo64rgfqyj.amplifyapp.com/#');
+            }else {
                 res.status(401).json({ success: false, message: 'Authentication failed' });
             }
         } else {
-            res.status(404).send('User not found');
+            res.redirect('http://localhost:8000/');
         }
     } catch (err) {
         console.error(err);
@@ -108,6 +156,71 @@ app.post('/login', async (req, res) => {
         await sql.close();
     }
 });
+
+async function isAuthenticated(req, res, next) {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+        return res.status(401).send('Unauthorized');
+    }
+
+    if (!verifyJWT(token, secretKey)) {
+        return res.status(401).send('Unauthorized');
+    }
+
+    // Check if the token is in the database
+    try {
+        await sql.connect(config);
+        const query = `SELECT * FROM mock_Accounts WHERE token = @token`;
+        const request = new sql.Request();
+        request.input('token', sql.VarChar, token);
+        const result = await request.query(query);
+
+        if (result.recordset.length === 0) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        next();
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+}
+
+app.get('/protected-route', (req, res) => {
+    const token = req.query.token;
+    if (token && verifyJWT(token, secretKey)) {
+        //res.redirect('https://main.dzyoo64rgfqyj.amplifyapp.com/#');
+        res.sendFile(path.join(__dirname, 'public2', 'index.html'));
+    } else {
+        res.status(401).send('Unauthorized');
+    }
+});
+
+app.post('/logout', async (req, res) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    try {
+        await sql.connect(config);
+        const updateDeleteTokenQuery = `
+                    UPDATE [dbo].[mock_Accounts]
+                    SET token = NULL where token = @token
+                    `;
+        const request = new sql.Request();
+        request.input('token', sql.VarChar, token);
+        const result = await request.query(updateDeleteTokenQuery);
+
+        // After deleting the token from the database, respond to the client
+        res.status(200).send('Logged out');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
+
+
+
+
 
 
 const server = app.listen(8000, function () {
