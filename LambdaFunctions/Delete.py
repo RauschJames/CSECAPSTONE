b2abe8b91c06340a5918d6b992cb6836ef3650f3
RauschJames <76003622+RauschJames@users.noreply.github.com>
2024-01-25 07:44:46 -0700

    Update Delete.py
    Updated to Match Alex' STS Method
    
    Signed-off-by: RauschJames <76003622+RauschJames@users.noreply.github.com>

--- LambdaFunctions/Delete.py
+++ LambdaFunctions/Delete.py
@@ -3,15 +3,37 @@ import boto3
 from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
-    # Initialize S3 client
-    s3 = boto3.client('s3')
-
-    # Retrieve the bucket name from the query string parameters
+    # Extracting the bucket name, account ID, and role name from the query parameters
     bucket_name = event['queryStringParameters']['bucketName']
+    account_id = event['queryStringParameters']['accountId']
+    role_name = event['queryStringParameters']['roleName']
+
+    # Assume the role in the target account
+    sts_client = boto3.client('sts')
+    assumed_role = sts_client.assume_role(
+        RoleArn=f'arn:aws:iam::{account_id}:role/{role_name}',
+        RoleSessionName='AssumeRoleSession1'
+    )
+
+    # Initialize S3 client with assumed role credentials
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
+        aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
+        aws_session_token=assumed_role['Credentials']['SessionToken']
+    )
 
     try:
-        # Attempt to delete the bucket
+        # Delete all objects and versions in the bucket before deleting the bucket itself
+        bucket = boto3.resource(
+            's3',
+            aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
+            aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
+            aws_session_token=assumed_role['Credentials']['SessionToken']
+        ).Bucket(bucket_name)
         bucket.object_versions.delete()
+
+        # Attempt to delete the bucket
         response = s3.delete_bucket(Bucket=bucket_name)
         return {
             'statusCode': 200,
