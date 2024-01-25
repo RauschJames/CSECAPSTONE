e3a71d1da35e8d1ce8eca46599f404e13f3b1903
RauschJames <76003622+RauschJames@users.noreply.github.com>
2024-01-25 15:21:49 -0700

    38 update previous adddelete to use authentication (#39)
    * Update Add.py
    
    Updated to use AWS Method Alex Uses
    
    Signed-off-by: RauschJames <76003622+RauschJames@users.noreply.github.com>
    
    * Update Delete.py
    
    Updated to Match Alex' STS Method
    
    Signed-off-by: RauschJames <76003622+RauschJames@users.noreply.github.com>
    
    * Update Add.py
    
    Added per Alex recommendation 
    
    Signed-off-by: RauschJames <76003622+RauschJames@users.noreply.github.com>
    
    * Update Delete.py
    
    Signed-off-by: RauschJames <76003622+RauschJames@users.noreply.github.com>
    
    * Update Add.py
    
    Signed-off-by: RauschJames <76003622+RauschJames@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: RauschJames <76003622+RauschJames@users.noreply.github.com>

--- LambdaFunctions/Add.py
+++ LambdaFunctions/Add.py
@@ -1,13 +1,30 @@
 #James Rausch
 import boto3
 import json
+lambda_region = 'us-west-2'
 
+boto3.setup_default_session(region_name=lambda_region) #added 
 def lambda_handler(event, context):
-    # Extracting the bucket name from the query parameters
+    # Extracting the bucket name, account ID, and role name from the query parameters
     bucket_name = event['queryStringParameters']['bucketName']
-
-    # Create an S3 client
-    s3 = boto3.client('s3')
+    account_id = event['queryStringParameters']['accountId']
+    role_name = event['queryStringParameters']['roleName']
+    
+    # Assume the role in the target account
+    sts_client = boto3.client('sts')
+    assumed_role = sts_client.assume_role(
+        RoleArn=f'arn:aws:iam::{account_id}:role/{role_name}',
+        RoleSessionName='AssumeRoleSession1'
+    )
+    s3_region = get_bucket_region(bucket_name, assumed_role)
+    # Create an S3 client using the assumed role's credentials
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
+        aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
+        aws_session_token=assumed_role['Credentials']['SessionToken']
+        config=boto3.session.Config(signature_version='s3v4',region_name=s3_region)
+    )
 
     # Create the bucket
     try:
