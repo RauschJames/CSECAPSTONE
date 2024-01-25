04d6edb980826a3d09fbf294e8c16759789519e5
RauschJames <76003622+RauschJames@users.noreply.github.com>
2024-01-25 07:43:04 -0700

    Update Add.py
    Updated to use AWS Method Alex Uses
    
    Signed-off-by: RauschJames <76003622+RauschJames@users.noreply.github.com>

--- LambdaFunctions/Add.py
+++ LambdaFunctions/Add.py
@@ -3,11 +3,25 @@ import boto3
 import json
 
 def lambda_handler(event, context):
-    # Extracting the bucket name from the query parameters
+    # Extracting the bucket name, account ID, and role name from the query parameters
     bucket_name = event['queryStringParameters']['bucketName']
-
-    # Create an S3 client
-    s3 = boto3.client('s3')
+    account_id = event['queryStringParameters']['accountId']
+    role_name = event['queryStringParameters']['roleName']
+    
+    # Assume the role in the target account
+    sts_client = boto3.client('sts')
+    assumed_role = sts_client.assume_role(
+        RoleArn=f'arn:aws:iam::{account_id}:role/{role_name}',
+        RoleSessionName='AssumeRoleSession1'
+    )
+    
+    # Create an S3 client using the assumed role's credentials
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
+        aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
+        aws_session_token=assumed_role['Credentials']['SessionToken']
+    )
 
     # Create the bucket
     try:
