dd4d42d5d687e1e20a15bb6d15b43c1353937d50
COEXIST\ARotteveel <Alexander.Rotteveel@Drivetime.com>
2024-02-06 13:48:52 -0700

    display bucket data, create session cookie, create sql queries for fetching new data, updatate registration and login page

--- node.js
+++ node.js
@@ -1,51 +1,27 @@
 //Varun Ravi Kumar
 const express = require('express');
-const sql = require('mssql');
+const mysql = require('mysql');
 const path = require('path'); 
 const app = express();
-const crypto = require('crypto');
-
+const session = require('express-session');
 
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
 const config = {
-    user: 'admin',
-    password: 'admin123',
-    server: 'mocked-db.cailplyo9oeg.us-east-1.rds.amazonaws.com', 
-    database: 'Accounts',
-    port: 1433,
-    options: {
-        encrypt: true, 
-        trustServerCertificate: true 
-    }
+    user: 'sql3680058',
+    password: 'r713z9gGLf',
+    host: 'sql3.freemysqlhosting.net',
+    database: 'sql3680058',
+    port: 3306
 };
-
-
-function createJWT(payload, secret) {
-    const header = Buffer.from(JSON.stringify({ alg: 'HS256', typ: 'JWT' })).toString('base64');
-    const encodedPayload = Buffer.from(JSON.stringify(payload)).toString('base64');
-    const signature = crypto.createHmac('sha256', secret).update(`${header}.${encodedPayload}`).digest('base64');
-    return `${header}.${encodedPayload}.${signature}`;
-}
-
-function verifyJWT(token, secret) {
-    const [header, payload, signature] = token.split('.');
-    const expectedSignature = crypto.createHmac('sha256', secret).update(`${header}.${payload}`).digest('base64');
-    return signature === expectedSignature;
-}
-
-const secretKey = 'your-secret-key'; // Use a strong, secret key
-
-function isAuthenticated(req, res, next) {
-    const token = req.headers.authorization;
-    if (token && verifyJWT(token.split(' ')[1], secretKey)) {
-        return next();
-    }
-    return res.status(401).send('Unauthorized');
-}
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'LoginPage.html'));
@@ -55,174 +31,145 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'registration.html'));
 });
 
-
-app.post('/submit-form', async (req, res) => {
-    
-    console.log(req.body);
-
-    
-    const { username, dob, hometown, gender, password } = req.body;
-
-    try {
-        await sql.connect(config);
-
-        const insertQuery = `
-            INSERT INTO [dbo].[mock_Accounts] ([username], [DateOfBirth], [Hometown], [Gender], [Password])
-            VALUES (@username, @dob, @hometown, @gender, @password)
-        `; 
-
-        const request = new sql.Request();
-        request.input('username', sql.VarChar, username);
-        request.input('dob', sql.Date, dob);
-        request.input('hometown', sql.VarChar, hometown);
-        request.input('gender', sql.VarChar, gender);
-        request.input('password', sql.VarChar, password); 
-
-        await request.query(insertQuery);
-        res.redirect('http://localhost:8000/');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error: ' + err.message);
-    } finally {
-        await sql.close();
-    }
+app.get('/ServiceContext', (req, res) => {
+    req.session.bucket_name = req.query.bucket_name;
+    req.session.bucket_owner_id = req.query.bucket_owner_id;
+    req.session.bucket_owner_role = req.query.bucket_owner_role;
+    res.sendFile(path.join(__dirname, 'ServiceContext.html'));
 });
 
+app.get('/getBucketData', (req, res) => {
+    res.json(req.session);
+});
+
+app.post('/submit-form', (req, res) => {
+
+    const connection = mysql.createConnection(config);
+
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to the database:', err);
+            process.exit(1); // Exit the app if database connection is critical
+        } else {
+            console.log('Connected to the MySQL database.');
+        }
+    });
+    
+    const { account_id, dob, hometown, gender, password, passwordAgain, role } = req.body;
+
+    if(password !== passwordAgain) {
+        connection.end((err) => {
+            if (err) {
+              console.error('Error closing the connection: ', err);
+            } else {
+              console.log('Connection closed successfully.');
+            }
+        });
+        res.redirect('/register');
+    }
+    
+    else {
+        const insertQuery = `
+            INSERT INTO user_accounts (account_id, password, role_name, hometown, gender, dob)
+            VALUES (?, ?, ?, ?, ?, ?)
+            `; 
+
+        const queryValues = [account_id, password, role, hometown, gender, dob];
+
+        connection.query(insertQuery, queryValues, (err, results) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Server error: ' + err.message);
+            } else {
+                res.redirect('/');
+            }
+        });
+    
+        connection.end((err) => {
+            if (err) {
+                console.error('Error closing the connection: ', err);
+            } else {
+                console.log('Connection closed successfully.');
+            }   
+        });
+    }
+});
 
 app.post('/login', async (req, res) => {
-  
-    //console.log(req.body);
-
     
-    const { username, password } = req.body;
+    const { account_id, password } = req.body;
 
+    const connection = mysql.createConnection(config);
 
-    try {
-        await sql.connect(config);
-
-        const query = `
-            SELECT [Password]
-            FROM [dbo].[mock_Accounts]
-            WHERE [username] = @username
-        `;
-
-        const request = new sql.Request();
-        request.input('username', sql.VarChar, username);
-
-        const result = await request.query(query);
-
-        if (result.recordset.length > 0) {
-            const storedPassword = result.recordset[0].Password;
-            
-            const isMatch = password === storedPassword; 
-
-            if (isMatch) {
-                // Create JWT
-                const token = createJWT({ username }, secretKey);
-                
-                await sql.connect(config);
-                const updateTokenQuery = `
-                    UPDATE [dbo].[mock_Accounts]
-                    SET token = @token
-                    WHERE username = @username
-                    `;
-
-        
-                const tokenRequest = new sql.Request();
-                tokenRequest.input('username', sql.VarChar, username);
-                tokenRequest.input('token', sql.VarChar, token);
-                await tokenRequest.query(updateTokenQuery);
-                
-                const redirectUrl = `http://localhost:8000/protected-route?token=${encodeURIComponent(token)}`;
-                //const redirectUrl = `https://main.dzyoo64rgfqyj.amplifyapp.com/protected-route?token=${encodeURIComponent(token)}`;
-                
-                res.redirect(redirectUrl);
-                
-                
-                //res.redirect('http://localhost:8000/protected-route');
-            //if (isMatch) {
-                //res.redirect('http://localhost:8000/index');
-              //  res.redirect('https://main.dzyoo64rgfqyj.amplifyapp.com/#');
-            }else {
-                res.status(401).json({ success: false, message: 'Authentication failed' });
-            }
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to the database:', err);
+            process.exit(1); // Exit the app if database connection is critical
         } else {
-            res.redirect('http://localhost:8000/');
+            console.log('Connected to the MySQL database.');
         }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error: ' + err.message);
-    } finally {
-        await sql.close();
-    }
+    });
+
+    const query = `
+        SELECT password, role_name
+        FROM user_accounts
+        WHERE account_id = ?
+    `;
+
+    connection.query(query, [account_id], (err, results) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Server error: ' + err.message);
+            return;
+        }
+        else {
+            if(results.length === 1) {
+                const storedPassword = results[0].password;
+                const isMatch = password === storedPassword; 
+
+                if(isMatch) {
+                    req.session.account_id = account_id;
+                    req.session.role_name = results[0].role_name;
+                    res.redirect('/index');
+                }
+                else {
+                    res.redirect('/');
+                }
+            }
+            else {
+                res.redirect('/');
+            }
+        } 
+    });
+        
+    connection.end((err) => {
+        if (err) {
+            console.error('Error closing the connection: ', err);
+        } else {
+            console.log('Connection closed successfully.');
+        }
+    });
 });
 
-async function isAuthenticated(req, res, next) {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) {
-        return res.status(401).send('Unauthorized');
-    }
-
-    if (!verifyJWT(token, secretKey)) {
-        return res.status(401).send('Unauthorized');
-    }
-
-    // Check if the token is in the database
-    try {
-        await sql.connect(config);
-        const query = `SELECT * FROM mock_Accounts WHERE token = @token`;
-        const request = new sql.Request();
-        request.input('token', sql.VarChar, token);
-        const result = await request.query(query);
-
-        if (result.recordset.length === 0) {
-            return res.status(401).send('Unauthorized');
-        }
-
-        next();
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
-    }
-}
-
-app.get('/protected-route', (req, res) => {
-    const token = req.query.token;
-    if (token && verifyJWT(token, secretKey)) {
-        //res.redirect('https://main.dzyoo64rgfqyj.amplifyapp.com/#');
-        res.sendFile(path.join(__dirname, 'public2', 'index.html'));
-    } else {
-        res.status(401).send('Unauthorized');
-    }
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public2', 'index.html'));
 });
 
 app.post('/logout', async (req, res) => {
-    const token = req.headers.authorization?.split(' ')[1];
-
-    try {
-        await sql.connect(config);
-        const updateDeleteTokenQuery = `
-                    UPDATE [dbo].[mock_Accounts]
-                    SET token = NULL where token = @token
-                    `;
-        const request = new sql.Request();
-        request.input('token', sql.VarChar, token);
-        const result = await request.query(updateDeleteTokenQuery);
-
-        // After deleting the token from the database, respond to the client
-        res.status(200).send('Logged out');
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
-    }
+    req.session.destroy((err) => {
+        if (err) {
+          console.error('Error destroying session:', err);
+          res.status(500).send('Server error');
+        } else {
+          res.redirect('/login'); 
+        }
+    });
 });
 
+app.get('/getSessionData', (req, res) => {
+    res.json(req.session);
+});
 
-
-
-
-
-
-const server = app.listen(8000, function () {
+app.listen(8000, function () {
     console.log('Server is running on port 8000');
 });
