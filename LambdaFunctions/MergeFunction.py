746afcb976eb800c3ec68961045ae827a4449a38
COEXIST\ARotteveel <Alexander.Rotteveel@Drivetime.com>
2024-02-17 23:52:30 -0700

    update merge function

--- LambdaFunctions/MergeFunction.py
+++ LambdaFunctions/MergeFunction.py
@@ -2,49 +2,56 @@
 import boto3
 import json
 
+lambda_region = 'us-west-2'
+
+boto3.setup_default_session(region_name=lambda_region)
+
+def get_bucket_region(bucket_name, assumed_role):
+    s3_client = boto3.client('s3',
+        aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
+        aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
+        aws_session_token=assumed_role['Credentials']['SessionToken']
+    )
+    
+    response = s3_client.get_bucket_location(Bucket=bucket_name)
+    return response.get('LocationConstraint', 'us-east-1')
+
 def assume_role(account_id, role_name):
     sts_client = boto3.client('sts')
     assumed_role = sts_client.assume_role(
         RoleArn=f'arn:aws:iam::{account_id}:role/{role_name}',
         RoleSessionName='AssumeRoleSession'
     )
-    return assumed_role['Credentials']
+    return assumed_role
 
-def get_s3_client(credentials, region_name):
-    return boto3.client(
-        's3',
-        region_name=region_name,
-        aws_access_key_id=credentials['AccessKeyId'],
-        aws_secret_access_key=credentials['SecretAccessKey'],
-        aws_session_token=credentials['SessionToken'],
-    )
+def get_s3_client(assumed_role, region):
+    s3_client = boto3.client('s3',
+        aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
+        aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
+        aws_session_token=assumed_role['Credentials']['SessionToken'],
+        config=boto3.session.Config(signature_version='s3v4', region_name=region))
+    
+    return s3_client
 
 def lambda_handler(event, context):
     try:
         # Extract necessary information from the event
-        source_bucket = event['bucket1']
-        destination_bucket = event['bucket2']
-        source_account_id = event.get('account')  # Optional, use if different from Lambda execution account
-        destination_account_id = event.get('account')  # Optional, use if different from Lambda execution account
-        role_name = event.get('role')  # Required if assuming a role
-        source_region = event.get('source_region', 'us-east-1')  # Default to us-east-1 if not provided
-        destination_region = event.get('destination_region', 'us-east-1')  # Default to us-east-1 if not provided
+        query_parameters = event.get('multiValueQueryStringParameters', {})
+        source_bucket = query_parameters.get('bucket2', {})
+        destination_bucket = query_parameters.get('bucket', {})
+        source_account_id = query_parameters.get('account2', {}) 
+        destination_account_id = query_parameters.get('account', {})  
+        role_name_source = query_parameters.get('role2', {})  
+        role_name_destination = query_parameters.get('role', {})  
 
-        # Assume role in source account if provided and different from Lambda execution account
-        if source_account_id:
-            source_credentials = assume_role(source_account_id, role_name)
-            s3_client_source = get_s3_client(source_credentials, source_region)
-        else:
-            s3_client_source = boto3.client('s3', region_name=source_region)
-
-        # Assume role in destination account if provided and different from Lambda execution account
-        if destination_account_id:
-            destination_credentials = assume_role(destination_account_id, role_name)
-            s3_client_destination = get_s3_client(destination_credentials, destination_region)
-        else:
-            s3_client_destination = boto3.client('s3', region_name=destination_region)
-
-        # List objects in the source bucket
+        source_role = assume_role(source_account_id, role_name_source)
+        source_region = get_bucket_region(source_bucket, source_role)
+        s3_client_source = get_s3_client(source_role, source_region)
+        
+        destination_role = assume_role(destination_account_id, role_name_destination)
+        destination_region = get_bucket_region(destination_bucket, destination_role)
+        s3_client_destination = get_s3_client(destination_role, destination_region)
+        
         response = s3_client_source.list_objects_v2(Bucket=source_bucket)
 
         # Copy each object to the destination bucket
